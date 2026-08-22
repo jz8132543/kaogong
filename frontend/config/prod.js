8c4af6c18cfaa95d8245d623d45df7f11656b3b6
jz8132543 <i@dora.im>
2026-08-22 23:36:09 +0800

    fix: add missing taro config files

---
+++ frontend/config/prod.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    NODE_ENV: '"production"'
+  },
+  defineConstants: {
+  },
+  mini: {},
+  h5: {
+    /**
+     * WebpackChain 插件配置
+     * @docs https://taro-docs.jd.com/docs/next/config-detail#h5webpackchain
+     */
+    // webpackChain (chain) {
+    //   chain.plugin('analyzer')
+    //     .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin, [])
+    // }
+  }
+}
